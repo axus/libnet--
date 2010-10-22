94baa5145f8696ca5b1e86b6432515e1ac3a1b5b
Axus <axusgrad#gmail.com>
2010-10-22 09:48:20 -0400

    Add test_google source, to test library

---
+++ test_google/src/bin.MinGW.mak
@@ -0,0 +1,44 @@
+############################
+# Makefile for MSYS + MinGW
+############################
+# GNU C++ Compiler
+CC=g++
+
+SRCDIR=src
+BUILD=build
+
+# -mconsole: Create a console application
+# -mwindows: Create a GUI application
+# -Wl,--enable-auto-import: Let the ld.exe linker automatically import from libraries
+LDFLAGS= -mconsole -Wl,--enable-auto-import
+
+#Minimum Windows version: Windows XP, IE 6.01
+CPPFLAGS= -DMINGW -D_WIN32_WINNT=0x0500 -DWINVER=0x0500 -D_WIN32_IE=0x0601
+
+#SRC files in SRCDIR directory
+SRC=$(addprefix $(SRCDIR)/, $(SRCFILES))
+
+# Choose object file names from source file names
+OBJFILES=$(SRCFILES:.cpp=.o)
+OBJ=$(addprefix $(BUILD)/, $(OBJFILES))
+
+# All warnings, optimization level 3
+CFLAGS=-Wall -O3
+
+# Default target of make is "all"
+.all: all      
+all: $(BIN) $(LIBBIN)
+
+# Build object files with chosen options
+$(BUILD)/%.o: $(SRCDIR)/%.cpp
+	$(CC) $(CFLAGS) $(CPPFLAGS) $(INCLUDES) -o $@ -c $<
+
+# Build executable from objects and libraries to current directory
+$(BIN): $(OBJ)
+	$(CC) $^ $(LDFLAGS) $(LIBS) -o $@
+	
+# Remove object files and core files with "clean" (- prevents errors from exiting)
+RM=rm -f
+.clean: clean
+clean:
+	-$(RM) $(BIN) $(OBJ) core
---
+++ test_google/src/test_google.cpp
@@ -0,0 +1,71 @@
+
+#include <netclient.h>
+#include <cstdio>
+#include <string>
+
+using namespace std;
+
+//Sleep function
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <cunistd>
+    #define Sleep(n) usleep( n * 1000 )
+#endif
+
+//Constants
+const size_t sleepytime = 500;
+const size_t timeouttime = 5000;
+
+//Callbacks
+size_t print_pkt( netpacket* pkt, void *cb_data);
+
+//MAIN
+int main (int argc, char *argv[])
+{
+  
+    const string server = "www.google.com";
+    const short port = 80, lport = 13371;
+    const string http_request("GET /index.html HTTP/1.1\r\n\r\n");
+
+    //Create client
+    netclient Client;
+    
+    //Add callback for all received packets
+    Client.addCB( print_pkt, NULL);
+
+    //Create http GET packet        
+    unsigned char buffer[netbase::NET_MAX_RECV_SIZE];
+    size_t index=0;
+    strncpy((char*)(buffer + index), http_request.c_str(), http_request.length());
+    netpacket http_get_pkt( http_request.length(), buffer + index);
+
+    //Connect... to Google!
+    int connection;
+    connection = Client.doConnect(server.c_str(), port, lport);
+
+    //Send HTTP request... to Google!
+    Client.sendMessage( connection, http_get_pkt );
+    
+    //Loop until timeout
+    size_t passedtime = 0;
+    int rv = 0;
+    while (passedtime < timeouttime) {
+        Sleep(sleepytime);
+        rv = Client.run();
+        if (rv == 0) {
+            passedtime += sleepytime;
+        }
+    }
+
+    //Client.doDisconnect();
+    
+    return connection;
+}
+
+//Callback
+size_t print_pkt( netpacket* pkt, void *cb_data)
+{
+    cout << (const char *)(pkt->get_ptr()) << endl;
+    return 0;
+}
